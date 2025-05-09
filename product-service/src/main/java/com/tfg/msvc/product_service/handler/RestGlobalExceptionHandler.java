34d3e3db8f39bd73fba8c546842f3ba8a64940e8
PxyReaper <162600948+PxyReaper@users.noreply.github.com>
2025-05-09 20:58:21 +0200

    Create RestGlobalExceptionHandler.java
    Añadiendo RestControllerAdvice

---
+++ product-service/src/main/java/com/tfg/msvc/product_service/handler/RestGlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.tfg.msvc.product_service.handler;
+
+import com.tfg.msvc.product_service.controller.DTO.StatusDto;
+import com.tfg.msvc.product_service.exception.ProductNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class RestGlobalExceptionHandler {
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Void> handleProductNotFoundException(ProductNotFoundException e) {
+        log.error(e.getMessage());
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Void> handleException(Exception e , HttpServletRequest request) {
+        log.error("Fatal error, {},{},{}",e.getMessage(),e.getClass(),request.getRequestURI());
+        return ResponseEntity.internalServerError().build();
+    }
+}
